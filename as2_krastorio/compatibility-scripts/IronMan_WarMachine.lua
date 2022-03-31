30fbf311a5f4522746ff3ffcb6779339dcf0ee14
Arnaud Piroelle <piroelle.arnaud@gmail.com>
2022-03-31 09:09:25 +0200

    Fix global mods and data variables

--- as2_krastorio/compatibility-scripts/IronMan_WarMachine.lua
+++ as2_krastorio/compatibility-scripts/IronMan_WarMachine.lua
@@ -1,3 +1,6 @@
+local data = _G.data
+local mods = _G.mods
+
 -- Allow personal roboport in iron man armor
 if mods["IronMan_WarMachine"] then
     table.insert(data.raw["equipment-grid"]["iron-grid"].equipment_categories, "universal-equipment")
